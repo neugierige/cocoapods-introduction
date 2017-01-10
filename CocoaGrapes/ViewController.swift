9770e52a55a1a7e7ec2cef4c2bc5fce32a82ecbc
Luyuan Nathan <lxing.nyc@gmail.com>
2017-01-10 17:32:55 -0500

    playing with MarqueeLabel pod

--- CocoaGrapes/ViewController.swift
+++ CocoaGrapes/ViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 import SwiftGifOrigin
+import MarqueeLabel
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let dankImage = UIImage.gif(name: "dankgrapes")
         imageView.image = dankImage
+        
+        label.text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+        
     }
 }
 
