5462b6963e4907b6e8effc1e052ad63095897f8a
Luyuan Nathan <lxing.nyc@gmail.com>
2017-01-11 14:40:57 -0500

    completed exercise

--- CocoaGrapes/ViewController.swift
+++ CocoaGrapes/ViewController.swift
@@ -13,7 +13,7 @@ import MarqueeLabel
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label: MarqueeLabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +21,12 @@ class ViewController: UIViewController {
         imageView.image = dankImage
         
         label.text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
-        
+        label.tapToScroll = true
+        label.type = MarqueeLabel.MarqueeType.continuous
+        label.animationCurve = UIViewAnimationCurve.easeInOut
+        label.trailingBuffer = 10
+        label.speed = MarqueeLabel.SpeedLimit.duration(30)
+        label.leadingBuffer = 10
     }
 }
 
